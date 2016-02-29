7095cb5573f696c350d19659bcae52fae1a30d93
Paul Kim <paul_kim_1996@hotmail.com>
2016-02-29 00:33:26 -0800

    Grid Layout, generate random mines

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+
+import java.util.*;
+
+import java.awt.GridLayout;
 import java.awt.event.*;
+
 public class Minesweeper {
 
 	private JFrame frame;
@@ -7,41 +12,72 @@ public class Minesweeper {
 	private JLabel label;
 	private JButton button;
 	private ActionListener Mine;
-	
-	Minesweeper(){
+	private ActionListener NotMine;
+
+	Minesweeper() {
 	}
-	
-	public void Sweep(){
+
+	public void Sweep() {
 		setUp();
 	}
-	
-	private void setUp(){
+
+	private void setUp() {
 		frame = new JFrame("MineSweeper");
 		frame.setVisible(true);
-		frame.setSize(500,500);
+		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		panel = new JPanel();
-		panel.setLayout(null);
-		
-		label = new JLabel("Minsweeper");
-		label.setBounds(200,0,100,20);
-		panel.add(label);
-		
-		Mine = new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		panel.setLayout(new GridLayout(10, 10));
+
+		// Action if mine is clicked
+		Mine = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				JOptionPane.showMessageDialog(panel, "This is a mine");
 			}
 		};
-		
-		for(int i = 0; i < 100; i++){
+
+		// Action if mine is not clicked
+		NotMine = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(panel, "This is not a mine");
+			}
+		};
+
+		// Generate mines
+		List<Integer> mines = GenerateMines();
+		int iterate = 0;
+
+		// Create buttons
+		for (int i = 0; i < 100; i++) {
 			button = new JButton(" ");
-			button.addActionListener(Mine);
-			button.setBounds(0+(i*45),45,45,45);
+			if (i == mines.get(iterate) && iterate < 10) {
+				button.addActionListener(Mine);
+				if (iterate != 9) {
+					iterate++;
+				}
+			} else {
+				button.addActionListener(NotMine);
+			}
 			panel.add(button);
 		}
-		
+
 		frame.add(panel);
 	}
-	
+
+	private List<Integer> GenerateMines() {
+		Random random = new Random();
+		List<Integer> mines = new LinkedList<Integer>();
+
+		// Generate mines until 10 unique mines are generated
+		while (mines.size() < 10) {
+			int mine = random.nextInt(99);
+			if (!mines.contains(mine)) {
+				mines.add(mine);
+			}
+		}
+		Collections.sort(mines);
+		return mines;
+	}
+
 }
